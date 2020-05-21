43ecc82e71ba103e033f1d84c39bceba0073dbcf
Ridana <dariutja@gmail.com>
2020-05-21 11:57:02 +0300

    ее

--- попытка/Form1.Designer.cs
+++ попытка/Form1.Designer.cs
@@ -34,7 +34,6 @@
             this.ExitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.инструментыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.UpdateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.справкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.tabPage4 = new System.Windows.Forms.TabPage();
             this.label3 = new System.Windows.Forms.Label();
             this.Delete = new System.Windows.Forms.Button();
@@ -70,8 +69,7 @@
             // 
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.файлToolStripMenuItem,
-            this.инструментыToolStripMenuItem,
-            this.справкаToolStripMenuItem});
+            this.инструментыToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Size = new System.Drawing.Size(569, 24);
@@ -90,7 +88,7 @@
             // ExitToolStripMenuItem
             // 
             this.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem";
-            this.ExitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.ExitToolStripMenuItem.Size = new System.Drawing.Size(109, 22);
             this.ExitToolStripMenuItem.Text = "Выход";
             this.ExitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
             // 
@@ -105,15 +103,9 @@
             // UpdateToolStripMenuItem
             // 
             this.UpdateToolStripMenuItem.Name = "UpdateToolStripMenuItem";
-            this.UpdateToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.UpdateToolStripMenuItem.Size = new System.Drawing.Size(128, 22);
             this.UpdateToolStripMenuItem.Text = "Обновить";
             this.UpdateToolStripMenuItem.Click += new System.EventHandler(this.UpdateToolStripMenuItem_Click);
-            // 
-            // справкаToolStripMenuItem
-            // 
-            this.справкаToolStripMenuItem.Name = "справкаToolStripMenuItem";
-            this.справкаToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
-            this.справкаToolStripMenuItem.Text = "Справка";
             // 
             // tabPage4
             // 
@@ -124,7 +116,7 @@
             this.tabPage4.Controls.Add(this.label6);
             this.tabPage4.Location = new System.Drawing.Point(4, 22);
             this.tabPage4.Name = "tabPage4";
-            this.tabPage4.Size = new System.Drawing.Size(1072, 610);
+            this.tabPage4.Size = new System.Drawing.Size(561, 439);
             this.tabPage4.TabIndex = 3;
             this.tabPage4.Text = "DELETE";
             this.tabPage4.UseVisualStyleBackColor = true;
@@ -191,7 +183,7 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(1072, 610);
+            this.tabPage2.Size = new System.Drawing.Size(561, 439);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "INSERT";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -202,7 +194,7 @@
             this.Calculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.Calculate.ForeColor = System.Drawing.Color.Yellow;
             this.Calculate.ImageAlign = System.Drawing.ContentAlignment.TopRight;
-            this.Calculate.Location = new System.Drawing.Point(147, 91);
+            this.Calculate.Location = new System.Drawing.Point(70, 168);
             this.Calculate.Name = "Calculate";
             this.Calculate.Size = new System.Drawing.Size(100, 32);
             this.Calculate.TabIndex = 14;
@@ -215,7 +207,7 @@
             this.label11.AutoSize = true;
             this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label11.ForeColor = System.Drawing.Color.Yellow;
-            this.label11.Location = new System.Drawing.Point(225, 156);
+            this.label11.Location = new System.Drawing.Point(227, 180);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(67, 20);
             this.label11.TabIndex = 13;
@@ -224,7 +216,7 @@
             // 
             // textBox8
             // 
-            this.textBox8.Location = new System.Drawing.Point(409, 243);
+            this.textBox8.Location = new System.Drawing.Point(441, 193);
             this.textBox8.Name = "textBox8";
             this.textBox8.ReadOnly = true;
             this.textBox8.Size = new System.Drawing.Size(100, 20);
@@ -232,7 +224,7 @@
             // 
             // textBox9
             // 
-            this.textBox9.Location = new System.Drawing.Point(409, 208);
+            this.textBox9.Location = new System.Drawing.Point(441, 153);
             this.textBox9.Name = "textBox9";
             this.textBox9.ReadOnly = true;
             this.textBox9.Size = new System.Drawing.Size(100, 20);
@@ -240,7 +232,7 @@
             // 
             // textBox10
             // 
-            this.textBox10.Location = new System.Drawing.Point(409, 172);
+            this.textBox10.Location = new System.Drawing.Point(441, 110);
             this.textBox10.Name = "textBox10";
             this.textBox10.ReadOnly = true;
             this.textBox10.Size = new System.Drawing.Size(100, 20);
@@ -248,21 +240,21 @@
             // 
             // textBox7
             // 
-            this.textBox7.Location = new System.Drawing.Point(409, 121);
+            this.textBox7.Location = new System.Drawing.Point(13, 90);
             this.textBox7.Name = "textBox7";
             this.textBox7.Size = new System.Drawing.Size(100, 20);
             this.textBox7.TabIndex = 6;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(409, 84);
+            this.textBox2.Location = new System.Drawing.Point(13, 55);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 3;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(409, 47);
+            this.textBox1.Location = new System.Drawing.Point(13, 19);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 2;
@@ -270,8 +262,8 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.ForeColor = System.Drawing.Color.Red;
-            this.label8.Location = new System.Drawing.Point(226, 250);
+            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(277, 200);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(158, 13);
             this.label8.TabIndex = 11;
@@ -280,8 +272,8 @@
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.ForeColor = System.Drawing.Color.Red;
-            this.label9.Location = new System.Drawing.Point(229, 215);
+            this.label9.ForeColor = System.Drawing.Color.White;
+            this.label9.Location = new System.Drawing.Point(280, 160);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(155, 13);
             this.label9.TabIndex = 8;
@@ -290,8 +282,8 @@
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.ForeColor = System.Drawing.Color.Red;
-            this.label10.Location = new System.Drawing.Point(232, 179);
+            this.label10.ForeColor = System.Drawing.Color.White;
+            this.label10.Location = new System.Drawing.Point(283, 117);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(152, 13);
             this.label10.TabIndex = 7;
@@ -300,8 +292,8 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.ForeColor = System.Drawing.Color.Red;
-            this.label7.Location = new System.Drawing.Point(281, 54);
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(136, 26);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(103, 13);
             this.label7.TabIndex = 5;
@@ -312,7 +304,7 @@
             this.Insert.BackColor = System.Drawing.Color.Black;
             this.Insert.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.Insert.ForeColor = System.Drawing.Color.Yellow;
-            this.Insert.Location = new System.Drawing.Point(64, 35);
+            this.Insert.Location = new System.Drawing.Point(365, 43);
             this.Insert.Name = "Insert";
             this.Insert.Size = new System.Drawing.Size(87, 41);
             this.Insert.TabIndex = 4;
@@ -323,8 +315,8 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.ForeColor = System.Drawing.Color.Red;
-            this.label2.Location = new System.Drawing.Point(287, 91);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(136, 62);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(97, 13);
             this.label2.TabIndex = 1;
@@ -333,8 +325,8 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.ForeColor = System.Drawing.Color.Red;
-            this.label1.Location = new System.Drawing.Point(258, 128);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(136, 97);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(126, 13);
             this.label1.TabIndex = 0;
@@ -347,7 +339,7 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(561, 450);
+            this.tabPage1.Size = new System.Drawing.Size(561, 439);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "SELECT";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -355,11 +347,12 @@
             // listBox1
             // 
             this.listBox1.BackColor = System.Drawing.Color.Black;
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.listBox1.ForeColor = System.Drawing.Color.Yellow;
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(22, 53);
+            this.listBox1.Location = new System.Drawing.Point(8, 15);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(354, 342);
+            this.listBox1.Size = new System.Drawing.Size(354, 338);
             this.listBox1.TabIndex = 0;
             // 
             // tabControl1
@@ -371,14 +364,14 @@
             this.tabControl1.Location = new System.Drawing.Point(0, 24);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(569, 476);
+            this.tabControl1.Size = new System.Drawing.Size(569, 465);
             this.tabControl1.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(569, 500);
+            this.ClientSize = new System.Drawing.Size(569, 489);
             this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
@@ -403,7 +396,6 @@
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem ExitToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem справкаToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem инструментыToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem UpdateToolStripMenuItem;
         private System.Windows.Forms.TabPage tabPage4;
